2644ed64943a64b27cc31ca1b8c6cf19ec440f3b
LENOVO <MeutyaAzzahrani>
2024-04-01 16:28:46 +0700

    commit awal

---
+++ pertemuan ke-6/ContohKedua.cpp
@@ -0,0 +1,16 @@
+    #include <iostream>
+    using namespace std;
+
+    struct DetailAlamat {
+        string desa;
+        string kota;
+        DetailAlamat alamat;
+        int umur;       
+
+    struct Mahasiswa {
+        string nim;
+        string nama;
+        DetailAlamat alamat;
+        int umur;
+    };
+ 
