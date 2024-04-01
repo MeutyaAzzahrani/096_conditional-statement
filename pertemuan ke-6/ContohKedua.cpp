9657d80769ee5d4cd081f34b29213e1c7e11b0a6
LENOVO <MeutyaAzzahrani>
2024-04-01 16:41:43 +0700

    menambahkan struct DetailAlamat

--- pertemuan ke-6/ContohKedua.cpp
+++ pertemuan ke-6/ContohKedua.cpp
@@ -4,13 +4,10 @@
     struct DetailAlamat {
         string desa;
         string kota;
-        DetailAlamat alamat;
-        int umur;       
+    };
 
     struct Mahasiswa {
         string nim;
         string nama;
-        DetailAlamat alamat;
-        int umur;
-    };
+
  
